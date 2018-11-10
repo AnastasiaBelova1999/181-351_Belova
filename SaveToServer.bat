c1a911b686627c729b6e3d55c720fa6980b1aafb
AnastasiaBelova1999 <nastya-belova1999@mail.ru>
2018-11-10 10:54:30 +0300

    %DATE%

--- SaveToServer.bat
+++ SaveToServer.bat
@@ -4,7 +4,10 @@ git add *.sln
 git add *.vcxproj
 git add *.filters
 git add *.bat
-git add *.users
+git config user.name AnastasiaBelova1999
+git config user.email nastya-belova1999@mail.ru
 git commit -am %DATE% %TIME%
-git clone https://github.com/AnastasiaBelova1999/181-351_Belova
-git push 181-351_Belova master
+git status
+git log
+git remote add AnastasiaBelova1999 https://github.com/AnastasiaBelova1999/181-351_Belova
+git push polytech master
